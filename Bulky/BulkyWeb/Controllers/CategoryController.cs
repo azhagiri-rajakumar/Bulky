94de58a511020299bcb9248cbdcb62b3fd26b5a4
Azhagiri R <azhagiri.r@ibridgellc.com>
2025-09-24 18:29:12 +0530

    [Azhagiri] Added validation for category

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -19,9 +19,29 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();            
         }
     }
 }
