5ea824e95f06069392448d0557f531bacab2d4f9
Azhagiri R <azhagiri.r@ibridgellc.com>
2025-10-03 14:39:05 +0530

    [Azhagiri R] Added new project for razor

--- Bulky/BulkyWebRazor_Temp/Program.cs
+++ Bulky/BulkyWebRazor_Temp/Program.cs
@@ -1,9 +1,15 @@
+using BulkyWebRazor_Temp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
